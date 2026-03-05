a3c231358503efe06bf24d0a10c0a0a77d96f1ce
Your Name <you@example.com>
2026-03-05 14:38:21 +0300

    Fix database config for Render

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -18,8 +18,8 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite://db.sqlite3',
-        conn_max_age=600
-        # ssl_require=True, 
+        conn_max_age=600,
+        ssl_require=True 
     )
 }
 ALLOWED_HOSTS = ['deployment-3-skte.onrender.com', 'localhost', '127.0.0.1']
@@ -36,8 +36,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
