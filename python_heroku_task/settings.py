c9bb69983e8a70634ec71c8582e793513b2e3533
Your Name <you@example.com>
2026-03-05 14:25:55 +0300

    Fix database config for Render

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -22,6 +22,7 @@ DATABASES = {
         # ssl_require=True, 
     )
 }
+ALLOWED_HOSTS = ['deployment-3-skte.onrender.com', 'localhost', '127.0.0.1']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
